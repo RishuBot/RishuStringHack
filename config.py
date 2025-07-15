a04d852c737f99ae265f2ab44ca21d301d9a667f
Rishu <vishwakarmarishabh366@gmail.com>
2025-07-15 07:38:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config:
     API_ID = "14050586"
     API_HASH = "42a60d9c657b106370c79bb0a8ac560c"  
     TOKEN = os.environ.get("TOKEN")
-    MONGO_URL = ""
+    MONGO_URL = os.environ.get("MONGO_URL")
     START_PIC = "https://envs.sh/enO.jpg"
     SUDOERS = filters.user(["5738579437"])
